ba35838d2385f255f7c4fd35d828ff5f135ce229
Eleanor Young <developer@nausicaea.net>
2025-08-20 21:08:32 +0200

    feature: add a mixin into AbstractBlock.Settings

---
+++ src/main/java/net/nausicaea/paraburdoo/mixin/accessor/SettingsAccessor.java
@@ -0,0 +1,14 @@
+package net.nausicaea.paraburdoo.mixin.accessor;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.registry.RegistryKey;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin({AbstractBlock.Settings.class})
+public interface SettingsAccessor {
+    @Accessor
+    RegistryKey<Block> getRegistryKey();
+}
+
